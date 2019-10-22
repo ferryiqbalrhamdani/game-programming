5746e3ee0c5c328b8fce1bbcc5b245b05675b484
ferryiqbalrhamdani <rhamdani128@gmail.com>
2019-10-22 17:05:08 +0700

    acak kata

---
+++ acak kata/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    string kata = "";
+
+    cout << "Masukan kata : ";
+    cin >> kata;
+
+    cout << "\nJumlah huruf dari kata " << kata << " adalah : " << kata.size() << endl;
+
+    for (int i = 0; i < kata.size(); i++) {
+        swap(kata[rand() % kata.size()], kata[rand() % kata.size()]);
+    }
+
+    cout << "Hasil acak kata : " << kata << endl;
+    return 0;
+}
